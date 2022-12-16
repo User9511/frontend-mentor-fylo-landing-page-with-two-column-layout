55214b4a143e1b2a0bf9133d00920e66940d4cdc
Smgy94 <Shanemcgeown@gmail.com>
2022-12-16 22:38:00 +0000

    commit

--- app.js
+++ app.js
@@ -9,11 +9,37 @@ form.addEventListener("submit", (e) => {
   form.classList.remove("valid");
   if (eInput.value == "") {
     text.innerText = "Email can't be blank";
+    eInput.classList.add("error-border");
   } else if (!eInput.value.match(pattern)) {
     //if patter is not matched with user's enter value
     text.innerText = "Please enter a valid email";
+    eInput.classList.add("error-border");
   } else {
     form.classList.replace("error", "valid"); //replacing error class with valid class
     text.innerText = "Thank you, let's get started!";
+    eInput.classList.add("valid-border");
+  }
+});
+
+const form02 = document.getElementById("form-02"),
+  eInput02 = form02.querySelector(".input02"),
+  text02 = form02.querySelector(".text02");
+
+form02.addEventListener("submit", (e) => {
+  e.preventDefault(); //preventing form from submitting
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //Regex pattern to validate email
+  form02.classList.add("error");
+  form02.classList.remove("valid");
+  if (eInput02.value == "") {
+    text02.innerText = "Email can't be blank";
+    eInput02.classList.add("error-border");
+  } else if (!eInput02.value.match(pattern)) {
+    //if patter is not matched with user's enter value
+    text02.innerText = "Please enter a valid email";
+    eInput02.classList.add("error-border");
+  } else {
+    form02.classList.replace("error", "valid"); //replacing error class with valid class
+    text02.innerText = "Thank you, let's get started!";
+    eInput02.classList.add("valid-border");
   }
 });
