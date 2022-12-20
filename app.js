c4e0d20312cd211bdce215fe01dcb1f643f59f7b
Smgy94 <Shanemcgeown@gmail.com>
2022-12-20 17:09:33 +0000

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form"),
   eInput = form.querySelector(".input"),
-  text = form.querySelector(".text");
+  text01 = form.querySelector(".text01");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault(); //preventing form from submitting
@@ -8,15 +8,15 @@ form.addEventListener("submit", (e) => {
   form.classList.add("error");
   form.classList.remove("valid");
   if (eInput.value == "") {
-    text.innerText = "Email can't be blank";
+    text01.innerText = "Email can't be blank";
     eInput.classList.add("error-border");
   } else if (!eInput.value.match(pattern)) {
     //if patter is not matched with user's enter value
-    text.innerText = "Please enter a valid email";
+    text01.innerText = "Please enter a valid email";
     eInput.classList.add("error-border");
   } else {
     form.classList.replace("error", "valid"); //replacing error class with valid class
-    text.innerText = "Thank you, let's get started!";
+    text01.innerText = "Thank you, let's get started!";
     eInput.classList.add("valid-border");
   }
 });
